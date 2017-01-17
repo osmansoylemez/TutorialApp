14263f76245962b395ac3702a549fa648b3793af
Osman SÖYLEMEZ <osman_soylemez@kocsistem.com.tr>
2017-01-17 11:19:56 +0200

    Initial Commit

---
+++ TutorialApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TutorialApp
+//
+//  Created by KSMacbook on 17/01/17.
+//  Copyright © 2017 KOCSISTEM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ TutorialApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TutorialApp
+//
+//  Created by KSMacbook on 17/01/17.
+//  Copyright © 2017 KOCSISTEM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
